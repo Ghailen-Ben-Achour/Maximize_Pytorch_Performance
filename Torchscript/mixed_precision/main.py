daf67d8460459db53858ddd52992264fedd4fcb1
benachourghailen@gmail.com <benachourghailen@gmail.com>
2021-09-18 15:07:18 +0100

    main file

---
+++ Torchscript/mixed_precision/main.py
@@ -0,0 +1,57 @@
+import torch
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+from modules import CNN_scripted, CNN
+from torch import optim
+from torch import nn
+from argparse import ArgumentParser
+from train import train
+
+def get_args():
+    parser = ArgumentParser(description='Torchscript module')
+    parser.add_argument('--module', type=str, default='scripted')
+    parser.add_argument('--batch_size', type=int, default=100)
+    parser.add_argument('--epoch', type=int, default=2)
+    args = parser.parse_args()
+    return args
+
+
+args = get_args()
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+train_data = datasets.MNIST(
+    root = '../data',
+    train = True,                         
+    transform = ToTensor(), 
+    download = True,            
+)
+test_data = datasets.MNIST(
+    root = '../data', 
+    train = False, 
+    transform = ToTensor()
+)
+
+loaders = {
+    'train' : torch.utils.data.DataLoader(train_data, 
+                                          batch_size=args.batch_size, 
+                                          shuffle=True, 
+                                          num_workers=2),
+    
+    'test'  : torch.utils.data.DataLoader(test_data, 
+                                          batch_size=args.batch_size, 
+                                          shuffle=True, 
+                                          num_workers=2),
+}
+
+if (args.module == 'scripted'):
+  cnn = CNN_scripted().to(device)
+else:
+  cnn = CNN().to(device)
+
+optimizer = optim.Adam(cnn.parameters(), lr = 0.01) 
+loss_func = nn.CrossEntropyLoss()   
+num_epochs = args.epoch
+      
+train(num_epochs, cnn, loaders, loss_func, optimizer, device)
