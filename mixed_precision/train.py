6f0b4e55e4bb29b3ea8cc2ffdb2e01fc06daae83
benachourghailen@gmail.com <benachourghailen@gmail.com>
2021-09-23 15:10:12 +0100

    mixed precision training

--- mixed_precision/train.py
+++ mixed_precision/train.py
@@ -3,6 +3,37 @@ import torch
 import time
 
 def train(num_epochs, cnn, loaders, loss_func, optimizer, device):
+
+    cnn.train()
+        
+    # Train the model
+    total_step = len(loaders['train'])
+    total_time= time.time()   
+    for epoch in range(num_epochs):
+        
+        for i, (images, labels) in enumerate(loaders['train']):
+          # clear gradients for this training step   
+            optimizer.zero_grad() 
+            t0= time.process_time()
+            # gives batch data, normalize x when iterate train_loader
+            b_x = Variable(images).to(device)   # batch x
+            b_y = Variable(labels).to(device)   # batch y
+              
+            output = cnn(b_x)[0]
+            loss = loss_func(output, b_y)
+            
+                    
+            
+            # backpropagation, compute gradients 
+            loss.backward()    
+            # apply gradients             
+            optimizer.step()
+            if (i+1) % 100 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Elapsed: {:.4f}' 
+                       .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(), time.process_time() - t0))
+    print('total time:', time.time()-total_time)
+
+def train_mixed_prec(num_epochs, cnn, loaders, loss_func, optimizer, device):
     scaler = torch.cuda.amp.GradScaler()
 
     cnn.train()
@@ -35,3 +66,5 @@ def train(num_epochs, cnn, loaders, loss_func, optimizer, device):
                 print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Elapsed: {:.4f}' 
                        .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(), time.process_time() - t0))
     print('total time:', time.time()-total_time)
+
+
