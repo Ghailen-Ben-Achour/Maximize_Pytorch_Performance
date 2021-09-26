07327ee156191726ddd379fd0eb9d8c26a348f7e
benachourghailen@gmail.com <benachourghailen@gmail.com>
2021-09-26 12:45:19 +0100

    update

--- mixed_precision/main.py
+++ mixed_precision/main.py
@@ -46,7 +46,7 @@ loaders = {
                                           num_workers=2),
 }
 
-if (args.module == 'scripted'):
+if (args.sript_module == 'scripted'):
   cnn = CNN_scripted().to(device)
 else:
   cnn = CNN().to(device)
